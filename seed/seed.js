b97be57dd7481475b05881545db6afd13996d18f
Sam Kersebet <sam.kersebet@gmail.com>
2021-10-02 22:37:14 -0500

    wtf

---
+++ seed/seed.js
@@ -0,0 +1,33 @@
+import { connect, connection } from 'mongoose';
+import Trash, { deleteMany } from '../models/trash';
+
+connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+});
+
+const db = connection;
+db.on("error", console.error.bind(console, "console error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+const sample = (array) => {
+    return array[Math.floor(Math.random()*array.length)];
+}
+const seedDB = async () => {
+    await deleteMany({});
+    for (i =0; i <50;i++){
+        const random = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random() * 20) + 10
+        const camp = new Campground ({
+            location : `${cities[random].city}, ${cities[random].state}`,
+            title : `${sample(descriptors)} ${sample(places)}`,
+            image : 'https://source.unsplash.com/collection/483251',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed praesentium esse veniam obcaecati quidem tempore voluptatum nisi saepe laboriosam consequatur neque odit, et nesciunt possimus pariatur nostrum ipsa deleniti cupiditate?',
+            price
+        })
+        await camp.save();
+    }
+    
+}
